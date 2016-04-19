de516ab71583ec7fbaea5bbb8e8c54371efef44b
YadJosan <Yad.josan@yahoo.com>
2016-04-19 23:17:59 +0530

    Initial Commit

---
+++ simpleserver/server.js
@@ -0,0 +1,62 @@
+//Require Modules
+var http=require('http');
+var url=require('url');
+var path=require('path');
+var fs=require('fs');
+
+//Area of Mime Type
+var mimeTypes={
+    "html":"text/html",
+    "jpeg":"text/jpeg",
+    "jpg":"image/jpeg",
+    "png":"image/png",
+    "js":"text/javascript",
+    "css":"text/css"
+};
+
+//Create Server
+http.createServer(function(req,res) {
+    var uri=url.parse(req.url).pathname;
+    var fileName=path.join(process.cwd(),unescape(uri));
+    console.log('Loading'+uri);
+    var stats;
+   try
+    {
+        stats=fs.lstatSync(fileName);
+    }
+    catch(e)
+    {
+        res.writeHead(404,{'Content-type':'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+        
+    //check if file/directory
+    
+    if(stats.isFile())
+    {
+        var mimeType=mimeTypes[path.extname(fileName).split(".").reverse()[0]]
+        res.writeHead(200,{'Content-type':mimeType});
+        var fileStream=fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    }else if(stats.isDirectory())
+    {
+        res.writeHead(302,{
+            'Location':'Index.html'
+        });
+        res.end();
+    }else{
+        res.writeHead(500,{'Content-type':'text/plain'});
+        res.write('500 internal Error\n');
+        res.end();
+    }
+}).listen(3000);
+// http.createServer(function (req,res) {
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end('HelloWorld\n');
+// }).listen(1337,'127.0.0.1');
+
+// console.log('Server running at http://127.0.0.1:1337');
+
+
